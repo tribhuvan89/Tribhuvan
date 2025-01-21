6c4a8d818074325094295fb8bca07e6e9519042b
Tribhuvan Pathak <144884917+tribhuvan89@users.noreply.github.com>
2025-01-21 20:19:03 +0530

    Add files via upload

---
+++ Star7.java
@@ -0,0 +1,14 @@
+public class Star7 {
+    public static void main(String[] args) {
+        int n=6 ;
+        int j;
+        for(int i=1;i<=n;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
